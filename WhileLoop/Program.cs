b70ada3206d6a74ce5a7cf071fa0435d4e9f8003
Greg Doud <gpdoud@gmail.com>
2021-07-14 11:35:16 -0400

    Committed by GitUtil

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WhileLoop {
+    class Program {
+        public string Print(string message) {
+            return message.ToUpper();
+        }
+        static void Main(string[] args) {
+
+            var nbr = 1;
+            while(nbr <= 25) {
+
+                if(nbr % 2 == 1) {
+                    Console.WriteLine($"The cube of {nbr} is {nbr * nbr * nbr}");
+                }
+
+                nbr++;
+            }
+        }
+    }
+}
